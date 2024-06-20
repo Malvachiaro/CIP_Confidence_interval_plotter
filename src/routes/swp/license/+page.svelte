e960c90ce8f61c69780ff3f8e8e2ee8b70cd291e
Malvachiaro <giacomo.scarpellini@gmail.com>
2024-06-20 11:49:02 +0200

    license fixed

--- src/routes/swp/license/+page.svelte
+++ src/routes/swp/license/+page.svelte
@@ -24,7 +24,7 @@
 
 <p>Copyright (c) 2024 Giacomo Scarpellini &lt;giacomo.scarpellini@gmail.com&gt;</p>
 
-<p> is hereby granted, free of charge, to any person obtaining a copy
+<p>Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
